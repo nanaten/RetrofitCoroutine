8db6700694ff47c6c1395ddab2886827ddcf5fef
m.coder <kurokoshow00@gmail.com>
2020-01-06 16:25:50 +0900

    fix Repos

--- app/src/main/java/com/nanaten/retrofitcoroutine/entity/Repos.kt
+++ app/src/main/java/com/nanaten/retrofitcoroutine/entity/Repos.kt
@@ -3,8 +3,6 @@ package com.nanaten.retrofitcoroutine.entity
 import com.google.gson.annotations.SerializedName
 
 data class Repos(
-    @SerializedName("id")
     val id: Int,
-    @SerializedName("name")
     val name: String
 )
